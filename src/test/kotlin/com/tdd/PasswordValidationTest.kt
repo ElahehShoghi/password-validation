106168d0d601e62f37286c06270d61d7d83ee9a4
Elaheh Shoghi <e.shoghi@rahkar.co>
2022-07-05 17:42:42 +0430

    Create the test case for checking that password must contain at least 2 numbers.

--- src/test/kotlin/com/tdd/PasswordValidationTest.kt
+++ src/test/kotlin/com/tdd/PasswordValidationTest.kt
@@ -9,7 +9,17 @@ class PasswordValidationTest {
 
     @Test
     fun shouldRaisePasswordLengthException_forPasswordsLessThan8Characters() {
-        val exception = assertFailsWith<PasswordValidation.PasswordLengthException> { passwordValidation.validate("1234") }
+        val exception = assertFailsWith<PasswordValidation.PasswordLengthException> {
+            passwordValidation.validate("1234")
+        }
         assertEquals("Password must be at least 8 characters", exception.message)
+    }
+
+    @Test
+    fun shouldRaiseNumberLengthException_forPasswordContainsLessThan2Numbers() {
+        val exception = assertFailsWith<PasswordValidation.NumberLengthException> {
+            passwordValidation.validate("1abcdfgjosup/")
+        }
+        assertEquals("The password must contain at least 2 numbers", exception.message)
     }
 }
