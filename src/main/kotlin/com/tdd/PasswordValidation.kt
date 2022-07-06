34fc764ff08e9445351e23306f06df0ac83babff
Elaheh Shoghi <e.shoghi@rahkar.co>
2022-07-06 16:47:01 +0430

    Pass the test.

--- src/main/kotlin/com/tdd/PasswordValidation.kt
+++ src/main/kotlin/com/tdd/PasswordValidation.kt
@@ -5,8 +5,13 @@ class PasswordValidation {
     fun validate(password: String): Boolean {
         if (password.length < 8)
             throw PasswordLengthException()
+        val digitCount = password.count() { it.isDigit() }
+        if (digitCount < 2)
+            throw NumberLengthException()
         return true
     }
 
     class PasswordLengthException : Exception("Password must be at least 8 characters")
+
+    class NumberLengthException : Exception("The password must contain at least 2 numbers")
 }
