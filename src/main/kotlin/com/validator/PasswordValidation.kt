8650e82a894c514620309e1dd96dbe0d1ca52b18
e.shoghi@fwutech.com <e.shoghi@fwutech.com>
2022-07-09 15:16:44 +0430

    Pass the test.

--- src/main/kotlin/com/validator/PasswordValidation.kt
+++ src/main/kotlin/com/validator/PasswordValidation.kt
@@ -1,24 +1,23 @@
 package com.validator
 
-import com.validator.exception.DigitCountValidationException
-import com.validator.exception.LengthValidationException
-import com.validator.exception.PasswordValidationException
-import com.validator.exception.UppercaseCountValidationException
+import com.validator.exception.*
 
 class PasswordValidation {
 
-    fun validate(password: String){
+    fun validate(password: String) {
         val isLengthNotValid = password.length < 8
         val isDigitCountNotValid = password.count { it.isDigit() } < 2
         val isUppercaseCountNotValid = password.count { it.isUpperCase() } < 1
-        if (isLengthNotValid || isDigitCountNotValid || isUppercaseCountNotValid)
-            throw customException(isLengthNotValid, isDigitCountNotValid, isUppercaseCountNotValid)
+        val isSpecialCharacterCountNotValid = password.count { !it.isLetterOrDigit() } < 1
+        if (isLengthNotValid || isDigitCountNotValid || isUppercaseCountNotValid || isSpecialCharacterCountNotValid)
+            throw customException(isLengthNotValid, isDigitCountNotValid, isUppercaseCountNotValid, isSpecialCharacterCountNotValid)
     }
 
     private fun customException(
             isLengthNotValid: Boolean,
             isDigitCountNotValid: Boolean,
-            isUppercaseCountNotValid: Boolean
+            isUppercaseCountNotValid: Boolean,
+            isSpecialCharacterCountNotValid: Boolean
     ): Exception {
         val exceptions = mutableListOf<Exception>()
         if (isLengthNotValid)
@@ -27,6 +26,8 @@ class PasswordValidation {
             exceptions.add(DigitCountValidationException())
         if (isUppercaseCountNotValid)
             exceptions.add(UppercaseCountValidationException())
+        if (isSpecialCharacterCountNotValid)
+            exceptions.add(SpecialCharactersCountNotValidationException())
         return PasswordValidationException(*exceptions.toTypedArray())
     }
 }
---
+++ src/main/kotlin/com/validator/exception/SpecialCharactersCountNotValidationException.kt
@@ -0,0 +1,3 @@
+package com.validator.exception
+
+class SpecialCharactersCountNotValidationException : Exception("password must contain at least one special character")
